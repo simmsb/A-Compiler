8e659ee5f3cc233f0aa8720ae22119cd1675da90
nitros12 <ben@bensimms.moe>
2017-09-25 14:36:24 +0100

    idk

--- wewv2_compiler/objects/irObject.py
+++ wewv2_compiler/objects/irObject.py
@@ -1,3 +1,18 @@
+from enum import IntEnum, auto
+
+class Instruction(IntEnum):
+    mov  = 0  # val * 4 + log2(width)  -> 0 * 4 + 1 = mov1, 1 * 4 + 3 = add4
+    add  = 1
+    mul  = 2
+    sub  = 3
+    udiv = 4  # unsigned divide
+    idiv = 5  # signed divide
+    psh  = 6
+    pop  = 7
+    sxt  = 8  # sign extend,  1 -> 2, 10000000 -> 1111111110000000
+    axt  = 9  # arith extend, 1 -> 2, 10000000 -> 0000000010000000
+
+
 class IRObject:
     """An instruction in internal representation
 
@@ -9,7 +24,9 @@ class IRObject:
 
     if params are insances of :class:`base.Variable` the variable is used appropriately
     """
-
-    def __init__(self, op, *params):  # TODO add types to this (op -> operation enum)
+    def __init__(self, op, size, *params):  # TODO add types to this (op -> operation enum)
         self.op, self.params = op, params
         self.object = None  # object is given to use when yielded
+
+    def emit(self):
+        return self.op * 4 + {1:1, 2:2, 4:3, 8:4}[self.size]
