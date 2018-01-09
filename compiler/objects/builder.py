simmsb/A-Compiler

commit d6b88b2026e0a4438f93aab48b115f112073062e

Fix: return statement, postfix operators
- Return statement: using the type of the function itself instead of function
  return type
- Postfix operators: Bug in parser generator means left recursion does not work:
  fix remove any left recursion and disable it, however this means all postfix
  operators have to be parenthesised like so: `((a-)-)-`

compiler/objects/builder.py
@@ -54,9 +54,6 @@ class WewSemantics(object):
     def return_stmt(self, ast):
         return ReturnStmt(ast)
 
-    def expression_stmt(self, ast):
-        return ast
-
     def expr(self, ast):
         return ast
 

compiler/parser/lang.ebnf
@@ -3,6 +3,8 @@
 @@comments :: /{~(\n|.)*~}/
 @@eol_comments :: /~~.*?$/
 
+@@left_recursion :: False
+
 @@parseinfo :: True
 
 start = {decl}+ $ ;
@@ -45,7 +47,7 @@ statement
     | loop_stmt
     | return_stmt
     ) [';']  # semicolon is optional for non expr-stmts
-  | expression_stmt ';'
+  | expr ';'
   ;
 
 scope
@@ -63,11 +65,6 @@ loop_stmt
 
 return_stmt = "return" ~ e:expr ;
 
-expression_stmt
-  =
-  expr
-  ;
-
 expr
   =
   | assign_expr
@@ -80,8 +77,8 @@ fun_decl
 
 optional_def
   =
-  | ":" typ:type ["=" val:expr]  # static
   | ":=" ~ val:expr typ:`infer`  # inferred
+  | ":" typ:type ["=" val:expr]  # static
   ;
 
 var_decl
@@ -197,12 +194,12 @@ postop
 
 postfix
   =
-  | left:postop '(' args:','.{expr} ')' type:`f`
-  | left:postop '[' ~ args:expr ']' type:`b`
-  | left:postop op:'++' type:`d`
-  | left:postop op:'--' type:`d`
-  | left:postop op:':::' ~ t:type type:`c`  # reinterpret expr
-  | left:postop op:'::' ~ t:type type:`c`  # cast expr
+  | left:singular '(' args:','.{expr} ')' type:`f`
+  | left:singular '[' ~ args:expr ']' type:`b`
+  | left:singular op:'++' type:`d`
+  | left:singular op:'--' type:`d`
+  | left:singular op:':::' ~ t:type type:`c`  # reinterpret expr
+  | left:singular op:'::' ~ t:type type:`c`  # cast expr
   ;
 
 singular

tests/test_parser.py
@@ -10,20 +10,20 @@ def test_var_declaration():
 
 def test_fn_declaration():
     decl = """fn b(a:[[*s4@3]@5], b:u2) -> u4 {
-        return 1
+        return 1;
     }"""
     parse_source(decl)
 
 
 def test_multiple_advanced():
     decl = """fn b(a:|[[|*s4|@3]@5]|, b:u2) -> |u4| {
-        var a: [u4@4] = {1, 2, 3, 4}
-        a = 5 * (4 + (4 / 3))
-        if a < b {print(wew, lad)}
+        var a: [u4@4] = {1, 2, 3, 4};
+        a = 5 * (4 + (4 / 3));
+        if a < b {print(wew, lad);}
         x++; ~~ we need a semicolon here
-        n = *(0:::*u2)
-        n[1+2]--
-        return f(a)
+        n = *(0:::*u2);
+        (n[1+2])--;
+        return f(a);
     }"""
     parse_source(decl)
 

compiler/objects/base.py
@@ -315,6 +315,7 @@ class FunctionDecl(Scope):
         self.name = ast.name
         self.params = {i[0]: Variable(i[0], i[2]) for i in ast.params}
         for var, offset in zip(self.params.values(), range(len(self.params), 0, -1)):
+            # TODO: Factor in the size of parameters
             var.stack_offset = -offset
         self._type = types.Function(ast.r, [i[2] for i in ast.params], True)
         # TODO: should functions be naturally const?

compiler/objects/literals.py
@@ -97,7 +97,8 @@ class ArrayLiteral(ExpressionObject):
 
     def to_array(self):
         """Convert type to array object from pointer object."""
-        self._type = types.Array(self._type.to, len(self.exprs))
+        to = (yield from self.exprs[0].type) if self._type is None else self._type.to
+        self._type = types.Array(to, len(self.exprs))
 
     def compile(self, ctx: CompileContext) -> ExprCompileType:
         #  this is only run if we're not in the form of a array initialisation.

compiler/objects/operations.py
@@ -105,7 +105,7 @@ class CastExprOP(ExpressionObject):
 
     def compile(self, ctx: CompileContext) -> ExprCompileType:
         reg: Register = (yield from self.expr.compile(ctx))
-        res = reg.resize(self._type.size, self._type.sign)
+        res = reg.resize(self._type.size, self._type.signed)
         if self.op == "::":
             ctx.emit(Resize(reg, res))  # emit resize operation
         else:

compiler/objects/statements.py
@@ -19,9 +19,6 @@ class VariableDecl(StatementObject):
         self._type = ast.typ
         self.val: Optional[ExpressionObject] = ast.val
 
-        if isinstance(self.val, ArrayLiteral):
-            self.val.to_array()
-
     @property
     def type(self) -> Generator[ObjectRequest, Variable, Type]:
         if self._type == "infer":
@@ -35,6 +32,8 @@ class VariableDecl(StatementObject):
         return self.name
 
     def compile(self, ctx: CompileContext) -> StmtCompileType:
+        if isinstance(self.val, ArrayLiteral):
+            yield from self.val.to_array()
         var = ctx.declare_variable(self.name, (yield from self.type))
         # TODO: but what about M u l t i - d i m e n s i o n a l arrays?
         if isinstance(self.val, ArrayLiteral):
@@ -60,11 +59,11 @@ class ReturnStmt(StatementObject):
         self.expr: ExpressionObject = ast.e
 
     def compile(self, ctx: CompileContext):
-        fn_type: Type = (yield from ctx.top_function.type)
+        fn_type = ctx.top_function.type
         expr_type: Type = (yield from self.expr.type)
 
-        if not fn_type.implicitly_casts_to(expr_type):
-            raise self.error(f"Return type '{expr_type}' cannot be casted to '{fn_type}'.")
+        if not fn_type.returns.implicitly_casts_to(expr_type):
+            raise self.error(f"Return type '{expr_type}' cannot be casted to '{fn_type.returns}'.")
 
         reg = yield from self.expr.compile(ctx)
         for i in reversed(ctx.scope_stack):
@@ -91,7 +90,7 @@ class IFStmt(StatementObject):
         else_jmp = end_jmp if self.else_ is None else JumpTarget()
         ctx.emit(Jump(else_jmp, CompType.eq))
         yield from self.body.compile(ctx)
-        if self.else_:  # if there is no else body, just jump to the end
+        if self.else_:  # if there is no else body, else_jmp = end_jmp so no need to emit anything but the end marker.
             ctx.emit(Jump(end_jmp, CompType.uncond))
             ctx.emit(else_jmp)
             yield from self.else_.compile(ctx)

compiler/objects/types.py
@@ -12,7 +12,7 @@ class Type:
         """Determine if it is valid to implicitly cast the the other type to this type.
         does not care about size.
         """
-        return any(isinstance(other, i) for i in self.can_cast_to)
+        return isinstance(other, self.can_cast_to)
 
 
 class Int(Type):

commit a697e077ba3389bf8f7480fdc04dd7b928f530f9

Fix regression caused by d6b88b2026
multiple postfix operators are now allowed.

compiler/objects/builder.py
@@ -118,8 +118,18 @@ class WewSemantics(object):
     def prefix(self, ast):
         return unary_prefix(ast)
 
+    def postfixexpr(self, ast):
+        return ast
+
     def postfix(self, ast):
-        return unary_postfix(ast)
+        # postfix gets a bit weird since we cant have left recursion
+        # instead we parse a list of expressions on the right hand side
+        # then we unfold this by generating ast nodes from left to right
+        final = ast.left
+        for i in ast.exprs:
+            i["left"] = final
+            final = unary_postfix(i)
+        return final
 
     def postop(self, ast):
         return ast

compiler/parser/lang.ebnf
@@ -192,14 +192,19 @@ postop
   | singular
   ;
 
+postfixexpr
+  =
+  | '(' args:','.{expr} ')' type:`f`
+  | '[' ~ args:expr ']' type:`b`
+  | op:'++' type:`d`
+  | op:'--' type:`d`
+  | op:':::' ~ t:type type:`c`  # reinterpret expr
+  | op:'::' ~ t:type type:`c`  # cast expr
+  ; 
+
 postfix
   =
-  | left:singular '(' args:','.{expr} ')' type:`f`
-  | left:singular '[' ~ args:expr ']' type:`b`
-  | left:singular op:'++' type:`d`
-  | left:singular op:'--' type:`d`
-  | left:singular op:':::' ~ t:type type:`c`  # reinterpret expr
-  | left:singular op:'::' ~ t:type type:`c`  # cast expr
+  left:singular exprs:{postfixexpr}
   ;
 
 singular

tests/test_parser.py
@@ -22,7 +22,7 @@ def test_multiple_advanced():
         if a < b {print(wew, lad);}
         x++; ~~ we need a semicolon here
         n = *(0:::*u2);
-        (n[1+2])--;
+        n[1+2]--;
         return f(a);
     }"""
     parse_source(decl)