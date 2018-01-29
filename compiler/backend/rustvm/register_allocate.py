ef3e4282bd95f423927ffc4ece4866f623bd1ca4
Ben Simms <ben@bensimms.moe>
2018-01-29 15:18:41 +0000

    Add: Something

--- compiler/backend/rustvm/register_allocate.py
+++ compiler/backend/rustvm/register_allocate.py
@@ -21,6 +21,7 @@ class RegisterState(Enum):
 class AllocationState:
     def __init__(self, register_count: int):
         self.register_count = register_count
+        self.usable_registers = set(range(register_count))
 
         #: the states of virtual registers, dict of Registers to Tuples of state and data
         self.register_states: Dict[Register, Tuple[RegisterState, Any]] = {}
@@ -28,13 +29,21 @@ class AllocationState:
         #: [x for x in list]st of extra memory places that are used to hold spilled registers
         self.spilled_registers: List[Optional[Register]] = []
 
-        #: the stack of allocated registers, contains tuples of (real register, virtual register)
-        self.allocated_registers: List[Tuple[int, Register]] = {}
+        #: the stack of allocated registers, k:v of real register to virtual register
+        self.allocated_registers: Dict[int, Register] = {}
 
-    def allocate_register(self, reg: Register):
-        ...
+    def allocate_register(self, v_reg: Register) -> int:
+        if v_reg in self.register_states:
+            state, data = self.register_states[v_reg]
+            if state is RegisterState.Allocated:
+                return data
 
-def find_usable_register(virtual_register: Register, spilled_registers, )
+        if self.usable_registers:
+            # best case, there is a register free to use.
+            reg = self.usable_registers.pop()
+            self.register_states[v_reg] = (RegisterState.Allocated, reg)
+            self.allocated_registers[reg] = v_reg
+            return reg
 
 
 def allocate(reg_count: int, code: Iterable[ir_object.IRObject]):
