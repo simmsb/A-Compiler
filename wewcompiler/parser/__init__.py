ed083339406f92cc70240be520f8950a995fb73b
Ben Simms <ben@bensimms.moe>
2018-04-24 22:35:07 +0100

    Load the parser directly if we can

--- wewcompiler/parser/__init__.py
+++ wewcompiler/parser/__init__.py
@@ -1,10 +1,13 @@
 import os
 
-import tatsu
-
 _lang = os.path.join(os.path.dirname(__file__), "lang.ebnf")
 
 with open(_lang) as f:
     language = f.read()
 
-lang = tatsu.compile(language)
+try:
+    from wewcompiler.parser.lang import WewParser
+    lang = WewParser()
+except ImportError:
+    import tatsu
+    lang = tatsu.compile(language)
